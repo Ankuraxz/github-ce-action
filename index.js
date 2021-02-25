a2dfa870283ffd9398fcfcb580728f19c12e35bd
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2021-02-25 12:44:54 -0500

    Our first push with custome code!

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const github = require('@actions/github');
 // most @actions toolkit packages have async methods
 async function run() {
   try {
+    const allowedUser = core.getInput('allowedUser');
+    core.info(allowedUser);
+
     // Get the event payload
+    const payload = github.context.payload;
+    core.info(payload);
 
     // Get an Octokit client and pass in our token
 
